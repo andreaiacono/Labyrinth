f369fc538b8dee7f769e43df5a985b36ca9eda89
Andrea Iacono <andrea.iacono.nl@gmail.com>
2022-11-04 00:25:19 +0100

    Update gh-pages

--- src/main/kotlin/maze/Generator.kt
+++
@@ -1,8 +0,0 @@
-package maze
-
-import org.w3c.dom.CanvasRenderingContext2D
-import utils.Params
-
-sealed interface Generator {
-    fun draw(params: Params, context: CanvasRenderingContext2D)
-}
--- src/main/kotlin/utils/Params.kt
+++
@@ -1,7 +0,0 @@
-package utils
-
-data class Params(
-    var backgroundColor: String,
-    var foregroundColor: String,
-    var lineWidth: Double
-)
